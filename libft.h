c1b7d1f6858acd5cc6c41f9a13f7a06aba661bbb
Li ren Chew <lchew@u90z04s06.42kl.edu.my>
2022-05-29 22:33:14 +0800

    comleated part 2

--- libft.h
+++ libft.h
@@ -19,8 +19,7 @@
 # include <string.h>
 # include <limits.h>
 
-typedef unsigned long long int	t_op;
-
+/* PART 1 */
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -49,12 +48,17 @@ int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 
-
+/* PART 2 */
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char 	**ft_split(char const *s, char c);
+char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
-
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
